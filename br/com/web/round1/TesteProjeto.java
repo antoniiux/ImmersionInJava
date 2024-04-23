13e9c7310f2145f1a94a002bce86f03b927e53a0
antoniiux <antoniiotales@gmail.com>
2024-04-23 03:15:59 -0300

    Variáveis, constantes e tipos de dados

--- br/com/web/round1/TesteProjeto.java
+++ br/com/web/round1/TesteProjeto.java
@@ -2,6 +2,24 @@ package br.com.web.round1;
 
 public class TesteProjeto {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
+        String nome = "Antonio";
+        int idade = 18;
+        float altura = 1.70f;
+        double peso = 80.5;
+        boolean testandoTudo = true;
+
+        var variavel = "Antonio tem 18 anos e estuda na Uniube";
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(peso);
+        System.out.println(testandoTudo);
+        System.out.println(variavel);
+
+        final var CONSTANTE = "testandoTudo";
+
+        System.out.println(CONSTANTE);
+
     }
 }
