9b0a653eb55563cfc5fcb09a81d98d7062aaf1df
antoniiux <antoniiotales@gmail.com>
2024-04-23 04:29:39 -0300

    Operadores de comparação

---
+++ br/com/web/round1/OperadoresDeComparacao.java
@@ -0,0 +1,24 @@
+package br.com.web.round1;
+
+public class OperadoresDeComparacao {
+    public static void main(String[] args) {
+
+        // Maior que
+        System.out.println(9 > 10);
+
+        // Menor que
+        System.out.println(5 < 9);
+
+        // Igualdade
+        System.out.println(10 == 110);
+
+        // Diferença
+        System.out.println(1 != 10);
+
+        // Maior Igual
+        System.out.println(2 >= 5);
+
+        // Menor igual
+        System.out.println(10 <= 10);
+    }
+}
