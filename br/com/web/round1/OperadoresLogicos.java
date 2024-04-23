382079acfd924a67335cd6dce6e4497f4f8ab23b
antoniiux <antoniiotales@gmail.com>
2024-04-23 04:53:59 -0300

    Operadores lógicos

---
+++ br/com/web/round1/OperadoresLogicos.java
@@ -0,0 +1,16 @@
+package br.com.web.round1;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        // NOT | NÃO
+        System.out.println(!(10 <= 10));
+
+        // AND | E
+        System.out.println(10 < 15 && 10 < 11);
+
+        // OR // OU
+        System.out.println(10 < 15 || 2 > 5);
+
+    }
+}
