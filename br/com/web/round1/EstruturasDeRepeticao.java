4b995ded897fa09dfe000ccfbff1e540ed063060
antoniiux <antoniiotales@gmail.com>
2024-04-24 15:00:42 -0300

    Estrutura de repetição: foreach

--- br/com/web/round1/EstruturasDeRepeticao.java
+++ br/com/web/round1/EstruturasDeRepeticao.java
@@ -1,5 +1,7 @@
 package br.com.web.round1;
 
+import java.util.ArrayList;
+
 public class EstruturasDeRepeticao {
     public static void main(String[] args) {
         // while e do-while
@@ -15,9 +17,25 @@ public class EstruturasDeRepeticao {
             contador++;
         } while (contador <= 1000);
 
-        // For
-        for (var contador2 = 1; contador2 <= 1000; contador2++) {
-            System.out.println("Número " + contador2);
+        // for
+        /*
+         * for (var contador2 = 1; contador2 <= 1000; contador2++) {
+         * System.out.println("Número " + contador2);
+         */
+
+        var notas = new ArrayList<Double>();
+        notas.add(10.0);
+        notas.add(8.1);
+        notas.add(0.5);
+        notas.add(0.2);
+
+        for (var index = 0; index < notas.size(); index++) {
+            System.out.println(notas.get(index));
+        }
+
+        // foreach
+        for (Double nota : notas) {
+            System.out.println(nota);
         }
     }
 }
