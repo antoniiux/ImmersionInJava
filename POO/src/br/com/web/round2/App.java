15b3c56a182dc52ce1a23cdfaba7385d3a130270
antoniiux <antoniiotales@gmail.com>
2024-04-24 16:20:18 -0300

    Monitoria: Ensinando as classe e Objetos no Java2

--- POO/src/br/com/web/round2/App.java
+++ POO/src/br/com/web/round2/App.java
@@ -4,10 +4,8 @@ import br.com.web.round2.classes.Diarista;
 
 public class App {
     public static void main(String[] args) {
-        var joao = new Diarista();
-        joao.nome = "João da Silva";
-        joao.telefone = "(34) 99777-7098";
-        joao.endereco = "Av Portugal, 1522";
+
+        var joao = new Diarista("João da Silva", "(34) 99777-7098", "Av Portugal, 1522");
 
         System.out.println("Nome: " + joao.nome);
         System.out.println("Telefone: " + joao.telefone);
@@ -15,14 +13,17 @@ public class App {
         joao.atender("Antonio");
         System.out.println();
 
-        var maria = new Diarista();
-        maria.nome = "Maria da Penha";
-        maria.telefone = "(34) 998774580";
-        maria.endereco = "Av Portugal, 1523";
+        var maria = new Diarista("Maria da Penha", "(34) 998774580", "Av Portugal, 1523");
 
         System.out.println("Nome: " + maria.nome);
         System.out.println("Telefone: " + maria.telefone);
         System.out.println("Endereço: " + maria.endereco);
         maria.atender("Hialen");
+
+        var gustavo = new Diarista("Gustavo Henrique da Silva");
+        System.out.println("Nome: " + gustavo.nome);
+        System.out.println("Telefone: " + gustavo.telefone);
+        System.out.println("Endereço: " + gustavo.endereco);
+
     }
 }
--- POO/src/br/com/web/round2/classes/Diarista.java
+++ POO/src/br/com/web/round2/classes/Diarista.java
@@ -6,6 +6,16 @@ public class Diarista {
     public String telefone;
     public String endereco;
 
+    public Diarista(String nome) {
+        this.nome = nome;
+    }
+
+    public Diarista(String nome, String telefone, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
     public void atender(String nomeCliente) {
         System.out.println("Realizando atendimento para o cliente " + nomeCliente);
     }
