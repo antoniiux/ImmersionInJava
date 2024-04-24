e81fd8268d5b465f6b343e824dbaacb4fe617988
antoniiux <antoniiotales@gmail.com>
2024-04-24 14:46:21 -0300

    Estrutura de repetição: while e do-while

---
+++ br/com/web/round1/EstruturasDeRepeticao.java
@@ -0,0 +1,18 @@
+package br.com.web.round1;
+
+public class EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        // while e do-while
+        var contador = 1001;
+        while (contador <= 1000) {
+            System.out.println("Número " + contador);
+            contador = contador + 1;
+            /* contador++ */
+        }
+
+        do {
+            System.out.println("Número " + contador);
+            contador++;
+        } while (contador <= 1000);
+    }
+}
