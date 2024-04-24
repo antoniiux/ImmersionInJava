4a55b837ecc655f2ee1050ef7cbd71b1727ee0e3
antoniiux <antoniiotales@gmail.com>
2024-04-24 14:37:07 -0300

    Collections Map

--- br/com/web/round1/Collections.java
+++ br/com/web/round1/Collections.java
@@ -1,17 +1,27 @@
 package br.com.web.round1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Collections {
     public static void main(String[] args) {
-
+        // List
         var notas = new ArrayList<Double>();
         notas.add(10.0);
         notas.add(8.1);
         notas.add(0.5);
 
         System.out.println(notas.size());
+        /* System.out.println(notas.get(0)) */
 
+        // Map
+        var medias = new HashMap<String, Double>();
+        medias.put("Floki", 10.0);
+        medias.put("Tom", 8.1);
+        medias.put("Elton", 0.5);
+
+        System.out.println(medias.get("Elton"));
+        /* System.out.println(medias); */
     }
 
 }
